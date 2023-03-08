e17f64a7e7d41291498afe385273ddddbde18475
Joe Letner <jrletner@gmail.com>
2023-03-08 09:48:53 -0600

    added CRUD

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,37 @@
 class BlogsController < ApplicationController
   def create
+    blog = Blog.new(title: params[:title], content: params[:content], image_path: params[:image_path])
+
+    # define response
+    if blog.save
+      render json: blog, status: :created
+    else
+      render json: blog.errors, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    blogs = Blog.all
+    render json: blogs, status: :ok
+  end
+
+  def show
+    blog = Blog.find(params[:id])
+    render json: blog, status: :ok
+  end
+
+  def update
+    blog = Blog.find(params[:id])
+    if blog.update(title: params[:title], content: params[:content], image_path: params[:image_path])
+      render json: blog, status: :ok
+    else
+      render json: blog.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    render json: { message: "Blog deleted" }, status: :ok
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # create a blog
   post "/blogs", to: "blogs#create"
+  # get all blogs
+  get "/blogs", to: "blogs#index"
+  # get a specific blog
+  get "/blogs/:id", to: "blogs#show"
+  # update a blog
+  put "/blogs/:id", to: "blogs#update"
+  # delete a blog
+  delete "/blogs/:id", to: "blogs#destroy"
 end
